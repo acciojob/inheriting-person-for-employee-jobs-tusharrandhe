b1421bb78041a96457543815fe548175b80dcd6a
Tushar Randhe <121237922+tusharrandhe@users.noreply.github.com>
2023-04-28 18:27:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ Employee.prototype.jobGreet = function() {
 	 console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 
-Employee.prototype = Object.extend(Person);
+Employee.prototype = Object.extend(new Person);
 
 // Do not change code below this line
 window.Person = Person;
