2e294992ea8967910e503776e9239798350817fb
Tushar Randhe <121237922+tusharrandhe@users.noreply.github.com>
2023-04-28 19:05:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 	
 function Employee(name, age, jobTitle) 
 {
-	this.__proto__ = new Person(name, age);
-	// this.name = name;
-	// this.age = age;
+	this.__proto__ = Person;
+	this.name = name;
+	this.age = age;
 	this.jobTitle = jobTitle;
 	// this.prototype.jobGreet = function()
 	// {
