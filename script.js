0883018e3cf893e6d801dd7f5c2d07e10e96f4ae
Tushar Randhe <121237922+tusharrandhe@users.noreply.github.com>
2023-04-28 14:52:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// complete this js code
 function Person(name, age) 
 {
 	this.name = name;
@@ -21,6 +20,9 @@ function Employee(name, age, jobTitle)
 	 console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 	}
 }
+
+const person = new Person("Alice", 25);
+const employee = new Employee("Bob", 30, "Manager");
 
 // Do not change code below this line
 window.Person = Person;
