eb6122b621a75343c704f2dca376a1ea0b107c64
Tushar Randhe <121237922+tusharrandhe@users.noreply.github.com>
2023-04-28 17:56:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function Person(name, age)
 {
 	this.name = name;
 	this.age = age;
-	// this.greet = function()
-	// {
-	//  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	// }
+	this.prototype.greet = function()
+	{
+	 console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	}
 }
 
-Person.prototype.greet = function() {
-console.log(Hello, my name is ${this.name}, I am ${this.age} years old.);
-}
+// Person.prototype.greet = function() {
+// console.log(Hello, my name is ${this.name}, I am ${this.age} years old.);
+// }
 
 	
 function Employee(name, age, jobTitle) 
@@ -18,14 +18,14 @@ function Employee(name, age, jobTitle)
 	this.name = name;
 	this.age = age;
 	this.jobTitle = jobTitle;
-	// this.jobGreet = function()
-	// {
-	//  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-	// }
-}
-Employee.prototype.jobGreet = function() {
+	this.prototype.jobGreet = function()
+	{
 	 console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+	}
 }
+// Employee.prototype.jobGreet = function() {
+// 	 console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+// }
 
 // Do not change code below this line
 window.Person = Person;
