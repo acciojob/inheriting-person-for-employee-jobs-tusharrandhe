c69a8b80415b138d2a129c8972b142604ec0d87c
Tushar Randhe <121237922+tusharrandhe@users.noreply.github.com>
2023-04-28 14:25:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,19 +3,18 @@
 function Person(name, age) {
 	this.name = name;
 	this.age = age;
-	greet(){
+	this.greet=
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 	}
-}
+
 	
 function Employee(name, age, jobTitle) {
 	this.__proto__ = Person;
 	this.name = name;
 	this.age = age;
 	this.jobTitle = jobTitle;
-	jobGreet(){
+	this.jobGreet=
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-	}
 }
 
 // Do not change code below this line
