3cabbba98351b6199ca0e8cc44ff5ade5be9a367
Tushar Randhe <121237922+tusharrandhe@users.noreply.github.com>
2023-04-29 10:55:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
-function Person(name, age) 
-{
-	this.name = name;
-	this.age = age;
-	// this.prototype.greet = function()
-	// {
-	//  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	// }
-}
+// function Person(name, age) 
+// {
+// 	this.name = name;
+// 	this.age = age;
+// 	// this.prototype.greet = function()
+// 	// {
+// 	//  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+// 	// }
+// }
 
+// Person.prototype.greet = function() {
+// console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+// }
+
+
+	
+// function Employee(name, age, jobTitle) 
+// {
+// 	this.__proto__ = Person;
+// 	this.name = name;
+// 	this.age = age;
+// 	this.jobTitle = jobTitle;
+// 	// this.prototype.jobGreet = function()
+// 	// {
+// 	//  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+// 	// }
+	
+// }
+// Employee.prototype.jobGreet = function() {
+// 	 console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+// }
+
+// // Employee.prototype = Object.extend(new Person);
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 Person.prototype.greet = function() {
-console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-}
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
 
 
-	
-function Employee(name, age, jobTitle) 
-{
-	this.__proto__ = Person;
-	this.name = name;
-	this.age = age;
-	this.jobTitle = jobTitle;
-	// this.prototype.jobGreet = function()
-	// {
-	//  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-	// }
-	
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
 Employee.prototype.jobGreet = function() {
-	 console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-}
-
-// Employee.prototype = Object.extend(new Person);
-
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
